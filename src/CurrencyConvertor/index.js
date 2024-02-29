30b01362e98662bcd358c634b66818a3ef99db04
sometextbtw <amtitkovv@gmail.com>
2024-03-01 01:30:45 +0300

    commit

--- src/CurrencyConvertor/index.js
+++ src/CurrencyConvertor/index.js
@@ -14,7 +14,7 @@ const Converter = () => {
     BYN: 0,
     RUB: 0,
     UAH: 0,
-    UAH: 0,
+    PLN: 0
   });
   const [selectedCurrency, setSelectedCurrency] = useState('');
   const baseCurrency = 'USD';
@@ -25,7 +25,8 @@ const Converter = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const menuButtonRef = useRef(null);
   const allCurrencies = Object.keys(amounts);
-  const [exchangeRates, setExchangeRates] = useState(null); 
+  const [exchangeRates, setExchangeRates] = useState(null);
+  const [currencyOrder, setCurrencyOrder] = useState(baseCurrencies);
 
   useEffect(() => {
     fetchExchangeRates();
@@ -54,23 +55,24 @@ const Converter = () => {
     return (
       <div className={`currency-rates ${isDarkMode ? 'dark-mode' : ''}`}>
         <style>
-          {isDarkMode && `
+          {isDarkMode &&
+            `
             .currency-rates ::-webkit-scrollbar {
               width: 12px;
             }
-    
+  
             .currency-rates ::-webkit-scrollbar-thumb {
               background-color: #888;
               border-radius: 6px;
             }
-    
+  
             .currency-rates ::-webkit-scrollbar-thumb:hover {
               background-color: #555;
             }
           `}
         </style>
         <h3 style={{ color: isDarkMode ? '#ddd' : '#222' }}>Курсы валют к 1 USD</h3>
-        {Object.keys(exchangeRates).map(currency => (
+        {availableCurrencies.map(currency => (
           <div key={currency} style={{ marginBottom: '20px' }}>
             <TextField
               label={currencies[currency]}
@@ -81,72 +83,78 @@ const Converter = () => {
               InputProps={{
                 readOnly: true,
                 startAdornment: (
-                  <InputAdornment position="start">
-                    {currency}
-                  </InputAdornment>
-                ),
+                  <InputAdornment position="start">{currency}</InputAdornment>
+                )
               }}
             />
           </div>
         ))}
       </div>
     );
-    
-    
-  
-    
   };
+  
 
   const handleAmountChange = (e, currency) => {
     let { value } = e.target;
     if (value === '' || parseFloat(value) === 0) {
-      value = '0.01';
+      value = '0';
     } else {
-      value = Math.max(parseFloat(value), 0.01);
+      value = Math.max(parseFloat(value), 0);
     }
     const newAmounts = { ...amounts, [currency]: value };
-  
+
     const baseAmount = parseFloat(value) / amounts[currency];
     for (const key in amounts) {
       if (key !== currency && baseCurrencies.includes(key)) {
         newAmounts[key] = (baseAmount * amounts[key]).toFixed(2);
       }
     }
-  
+
     updateAddedCurrencies(currency, value, newAmounts);
   };
-  
-  
-  const updateAddedCurrencies = async (baseCurrency, baseValue, newAmounts) => {
-    for (const key in newAmounts) {
-      if (!baseCurrencies.includes(key)) {
-        try {
-          const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${baseCurrency}`);
-          const data = await response.json();
-          const exchangeRate = data.rates[key];
-          const convertedAmount = (baseValue * exchangeRate).toFixed(2);
-          newAmounts[key] = parseFloat(convertedAmount);
-        } catch (error) {
-          console.error('Failed to fetch exchange rate:', error);
-        }
-      }
-    }
-  
-    setAmounts(newAmounts);
-  };
-  
 
-  const handleAddCurrency = async (currency) => {
+  const updateAddedCurrencies = async (baseCurrency, baseValue, newAmounts) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/update-added-currencies', {
+        // Указываем полный адрес сервера
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          baseCurrency,
+          baseValue,
+          newAmounts
+        })
+      });
+      const updatedAmounts = await response.json();
+      setAmounts(updatedAmounts);
+    } catch (error) {
+      console.error('Failed to update added currencies:', error);
+    }
+  };
+
+  const handleAddCurrency = async currency => {
     if (currency && !Object.keys(amounts).includes(currency)) {
       try {
-        const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${baseCurrency}`);
-        const data = await response.json();
-        const exchangeRate = data.rates[currency];
-        const convertedAmount = (amounts[baseCurrency] * exchangeRate).toFixed(2);
-        setAmounts({ ...amounts, [currency]: parseFloat(convertedAmount) });
+        const response = await fetch('http://localhost:5000/api/handle-add-currency', {
+          // Указываем полный адрес сервера
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            baseCurrency,
+            currency,
+            amounts
+          })
+        });
+        const updatedAmounts = await response.json();
+        setAmounts(updatedAmounts);
+        setCurrencyOrder([...currencyOrder, currency]); // Добавляем новую валюту в конец списка
         setMenuOpen(false);
       } catch (error) {
-        console.error('Failed to fetch exchange rate:', error);
+        console.error('Failed to handle add currency:', error);
       }
     }
   };
@@ -154,6 +162,7 @@ const Converter = () => {
   const handleRemoveCurrency = currency => {
     const { [currency]: value, ...newAmounts } = amounts;
     setAmounts(newAmounts);
+    setCurrencyOrder(currencyOrder.filter(curr => curr !== currency)); // Удаляем валюту из списка
   };
 
   const toggleDarkMode = () => {
@@ -161,7 +170,8 @@ const Converter = () => {
 
     const body = document.querySelector('body');
     if (isDarkMode) {
-      body.style.background = 'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)';
+      body.style.background =
+        'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)';
     } else {
       body.style.background = 'linear-gradient(-45deg, #222, #444, #666)';
     }
@@ -169,7 +179,9 @@ const Converter = () => {
 
   return (
     <div className={`container text-center ${isDarkMode ? 'dark-mode' : ''}`}>
-      <h2 style={{ display: 'inline-block', color: isDarkMode ? '#ddd' : '#222' }}>Конвертер валют</h2>
+      <h2 style={{ display: 'inline-block', color: isDarkMode ? '#ddd' : '#222' }}>
+        Конвертер валют
+      </h2>
       <button onClick={toggleDarkMode} className="toggle-theme-btn" style={{ float: 'right' }}>
         <Brightness4Icon />
       </button>
@@ -181,16 +193,16 @@ const Converter = () => {
         {currentTab === 0 && (
           <div>
             <div className="currency-list-container">
-              {Object.keys(amounts).map(currency => (
+              {currencyOrder.map(currency => (
                 <div className="form-group" key={currency}>
                   <div style={{ display: 'flex', alignItems: 'center' }}>
                     <TextField
-                      value={currency} 
+                      value={currency}
                       disabled
-                      style={{ fontWeight: 'bold', width: '150px' }} 
+                      style={{ fontWeight: 'bold', width: '150px' }}
                     />
                     <TextField
-                      label={currencies[currency]} 
+                      label={currencies[currency]}
                       type="number"
                       value={amounts[currency]}
                       onChange={e => handleAmountChange(e, currency)}
@@ -221,7 +233,7 @@ const Converter = () => {
                 PaperProps={{
                   style: {
                     backgroundColor: isDarkMode ? '#333' : '',
-                    color: isDarkMode ? '#ddd' : '',
+                    color: isDarkMode ? '#ddd' : ''
                   }
                 }}
               >
@@ -234,11 +246,7 @@ const Converter = () => {
             </div>
           </div>
         )}
-        {currentTab === 1 && (
-          <div>
-            {renderCurrencyRates()}
-          </div>
-        )}
+        {currentTab === 1 && <div>{renderCurrencyRates()}</div>}
       </div>
     </div>
   );
--- src/CurrencyConvertor/server.py
+++ src/CurrencyConvertor/server.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 DATABASE = 'rates.db'
+CACHE = {}
 
 def create_db():
     conn = sqlite3.connect(DATABASE)
@@ -35,29 +36,36 @@ def get_exchange_rates_from_db():
     return {row[0]: row[1] for row in rows}
 
 def get_exchange_rates(base_currency):
-    url = f'https://api.exchangerate-api.com/v4/latest/{base_currency}'
-    
-    try:
-        response = requests.get(url)
-        data = response.json()
-        rates = data['rates']
-        insert_exchange_rates(rates)  
-        return rates
-    
-    except Exception as e:
-        return {'error': str(e)}
+    if CACHE.get(base_currency):
+        return CACHE[base_currency]
+
+    exchange_rates = get_exchange_rates_from_db()
+    if not exchange_rates:
+        url = f'https://api.exchangerate-api.com/v4/latest/{base_currency}'
+        try:
+            response = requests.get(url)
+            data = response.json()
+            rates = data['rates']
+            insert_exchange_rates(rates)
+            CACHE[base_currency] = rates
+            return rates
+        except Exception as e:
+            return {'error': str(e)}
+    else:
+        CACHE[base_currency] = exchange_rates
+        return exchange_rates
+
+def update_database():
+    base_currency = 'USD'
+    get_exchange_rates(base_currency)
 
 create_db()
 
-def update_database():
-    base_currency = 'USD'  
-    rates = get_exchange_rates(base_currency)
-
-schedule.every(2).hours.do(update_database)
+schedule.every().hour.do(update_database)
 
 @app.route('/api/exchange-rates')
 def get_exchange_rates_route():
-    exchange_rates = get_exchange_rates_from_db()  
+    exchange_rates = get_exchange_rates_from_db()
     return jsonify(exchange_rates)
 
 @app.route('/api/convert-currency', methods=['POST'])
@@ -67,12 +75,57 @@ def convert_currency_route():
     from_currency = data['from']
     to_currency = data['to']
     
-    exchange_rates = get_exchange_rates_from_db()  
+    exchange_rates = get_exchange_rates_from_db()
     if exchange_rates.get(from_currency) is None or exchange_rates.get(to_currency) is None:
         return jsonify({'error': 'Курс обмена не найден для предоставленных валют'})
     
     converted_amount = amount * exchange_rates[to_currency] / exchange_rates[from_currency]
     return jsonify({'converted_amount': converted_amount})
+
+@app.route('/api/update-added-currencies', methods=['POST'])
+def update_added_currencies_route():
+    data = request.get_json()
+    base_currency = data['baseCurrency']
+    base_value = float(data['baseValue'])
+    new_amounts = data['newAmounts']
+    
+    try:
+        response = requests.get(f'https://api.exchangerate-api.com/v4/latest/{base_currency}')
+        data = response.json()
+        rates = data['rates']
+        for key in new_amounts:
+            exchange_rate = rates.get(key)
+            if exchange_rate is not None:
+                converted_amount = round(base_value * exchange_rate, 2)
+                new_amounts[key] = converted_amount
+            else:
+                print(f'Failed to fetch exchange rate for {key}')
+        return jsonify(new_amounts)
+    except Exception as e:
+        print('Failed to fetch exchange rates:', e)
+        return jsonify(new_amounts)
+
+@app.route('/api/handle-add-currency', methods=['POST'])
+def handle_add_currency_route():
+    data = request.get_json()
+    base_currency = data['baseCurrency']
+    currency = data['currency']
+    amounts = data['amounts']
+    
+    try:
+        response = requests.get(f'https://api.exchangerate-api.com/v4/latest/{base_currency}')
+        data = response.json()
+        rates = data['rates']
+        exchange_rate = rates.get(currency)
+        if exchange_rate is not None:
+            converted_amount = round(amounts[base_currency] * exchange_rate, 2)
+            amounts[currency] = converted_amount
+        else:
+            print(f'Failed to fetch exchange rate for {currency}')
+        return jsonify(amounts)
+    except Exception as e:
+        print('Failed to fetch exchange rate:', e)
+        return jsonify(amounts)
 
 if __name__ == '__main__':
     app.run(debug=True)
