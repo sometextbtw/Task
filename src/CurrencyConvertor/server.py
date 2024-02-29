9771d56abcf8c42657e7242b904d6b16c2ef849a
sometextbtw <amtitkovv@gmail.com>
2024-02-29 23:04:01 +0300

    commit

--- src/CurrencyConvertor/server.py
+++ src/CurrencyConvertor/server.py
@@ -8,10 +8,8 @@ import time
 app = Flask(__name__)
 CORS(app)
 
-# Имя файла базы данных
 DATABASE = 'rates.db'
 
-# Функция для создания базы данных и таблицы, если она не существует
 def create_db():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
@@ -20,7 +18,6 @@ def create_db():
     conn.commit()
     conn.close()
 
-# Функция для вставки курсов валют в базу данных
 def insert_exchange_rates(rates):
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
@@ -29,7 +26,6 @@ def insert_exchange_rates(rates):
     conn.commit()
     conn.close()
 
-# Функция для получения курсов валют из базы данных
 def get_exchange_rates_from_db():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
@@ -38,7 +34,6 @@ def get_exchange_rates_from_db():
     conn.close()
     return {row[0]: row[1] for row in rows}
 
-# Функция для получения курсов валют из API и сохранения их в базе данных
 def get_exchange_rates(base_currency):
     url = f'https://api.exchangerate-api.com/v4/latest/{base_currency}'
     
@@ -46,30 +41,25 @@ def get_exchange_rates(base_currency):
         response = requests.get(url)
         data = response.json()
         rates = data['rates']
-        insert_exchange_rates(rates)  # Сохраняем полученные курсы в базе данных
+        insert_exchange_rates(rates)  
         return rates
     
     except Exception as e:
         return {'error': str(e)}
 
-# Создаем таблицу в базе данных при запуске приложения
 create_db()
 
-# Функция для обновления данных в базе данных каждые два часа
 def update_database():
-    base_currency = 'USD'  # Базовая валюта, можно изменить по необходимости
+    base_currency = 'USD'  
     rates = get_exchange_rates(base_currency)
 
-# Планирование задачи обновления базы данных каждые два часа
 schedule.every(2).hours.do(update_database)
 
-# Маршрут для получения курсов валют
 @app.route('/api/exchange-rates')
 def get_exchange_rates_route():
-    exchange_rates = get_exchange_rates_from_db()  # Получаем курсы валют из базы данных
+    exchange_rates = get_exchange_rates_from_db()  
     return jsonify(exchange_rates)
 
-# Маршрут для конвертации валюты
 @app.route('/api/convert-currency', methods=['POST'])
 def convert_currency_route():
     data = request.get_json()
@@ -77,18 +67,16 @@ def convert_currency_route():
     from_currency = data['from']
     to_currency = data['to']
     
-    exchange_rates = get_exchange_rates_from_db()  # Получаем курсы валют из базы данных
+    exchange_rates = get_exchange_rates_from_db()  
     if exchange_rates.get(from_currency) is None or exchange_rates.get(to_currency) is None:
         return jsonify({'error': 'Курс обмена не найден для предоставленных валют'})
     
     converted_amount = amount * exchange_rates[to_currency] / exchange_rates[from_currency]
     return jsonify({'converted_amount': converted_amount})
 
-# Запускаем сервер Flask
 if __name__ == '__main__':
     app.run(debug=True)
 
-# Запуск планировщика задач
 while True:
     schedule.run_pending()
     time.sleep(1)
